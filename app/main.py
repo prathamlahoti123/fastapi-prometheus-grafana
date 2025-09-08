bf73fe3537903ad8232937e87133da6e08819a24
prathamlahoti123 <prathamlahoti@duck.com>
2025-09-08 18:30:16 +0300

    Remove custom logging setup

--- app/main.py
+++ app/main.py
@@ -1,21 +1,6 @@
-import logging
-from typing import TYPE_CHECKING
-
 from fastapi import FastAPI, Response, status
 from fastapi.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
-
-if TYPE_CHECKING:
-  from logging import Logger
-
-
-def configure_logging() -> "Logger":
-  logging.basicConfig(
-    level=logging.INFO,
-    format="%(levelname)s - %(name)s - %(asctime)s - %(message)s",
-  )
-  return logging.getLogger(__name__)
-
 
 app = FastAPI()
 app.add_middleware(
