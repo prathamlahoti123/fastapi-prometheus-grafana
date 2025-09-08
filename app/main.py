4a169a065cb7667b28af95f30b4fa57c8f7e741c
prathamlahoti123 <prathamlahoti@duck.com>
2025-09-08 18:21:14 +0300

    Replace 'index' route with 'health'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import logging
 from typing import TYPE_CHECKING
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from fastapi.middleware.cors import CORSMiddleware
 from prometheus_fastapi_instrumentator import Instrumentator
 
@@ -28,6 +28,10 @@ app.add_middleware(
 Instrumentator().instrument(app).expose(app)
 
 
-@app.get("/")
-async def index() -> dict[str, str]:
-  return {"message": "Hello, World!"}
+@app.get("/health", status_code=status.HTTP_204_NO_CONTENT)
+async def health() -> Response:
+  """Health-check endpoint."""
+  return Response(
+    status_code=status.HTTP_204_NO_CONTENT,
+    headers={"x-status": "ok"},
+  )
