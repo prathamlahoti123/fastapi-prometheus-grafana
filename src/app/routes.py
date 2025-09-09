5d79c39e3c06c57432456500b27336ba2583b098
prathamlahoti123 <prathamlahoti@duck.com>
2025-09-09 17:42:26 +0300

    Remove integration with 'company_router'

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,8 +1,8 @@
 from fastcrud import crud_router
 
 from app.dependencies import get_db_session
-from app.models import Company, User
-from app.schemas import CreateCompany, CreateUser, UpdateCompany, UpdateUser
+from app.models import User
+from app.schemas import CreateUser, UpdateUser
 
 # API router to manage users
 user_router = crud_router(
@@ -12,14 +12,4 @@ user_router = crud_router(
   update_schema=UpdateUser,
   path="/users",
   tags=["users"],
-)
-
-# API router to manage companies
-company_router = crud_router(
-  session=get_db_session,
-  model=Company,
-  create_schema=CreateCompany,
-  update_schema=UpdateCompany,
-  path="/company",
-  tags=["companies"],
 )
